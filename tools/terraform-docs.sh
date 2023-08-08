16a39f2b2ba09790214c33928e0b8737a1c9941b
Kuno <gustavo.mkuno@gmail.com>
2023-08-08 09:46:11 -0300

    tools

---
+++ tools/terraform-docs.sh
@@ -0,0 +1,9 @@
+curl -sSLo ./terraform-docs.tar.gz https://terraform-docs.io/dl/v0.16.0/terraform-docs-v0.16.0-$(uname)-amd64.tar.gz
+tar -xzf terraform-docs.tar.gz
+chmod +x terraform-docs
+sudo mv terraform-docs /usr/local/bin
+
+
+
+#terraform-docs completion zsh > /usr/local/share/zsh/site-functions/_terraform-docs
+#autoload -U compinit && compinit
